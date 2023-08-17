7245666e4daf04bb9964e315f4e04d03e4c03573
nadiropu <nadir.swtest@gmail.com>
2023-08-18 00:13:29 +0600

    Implement encapsulation Feature

---
+++ src/Encapsulation.java
@@ -0,0 +1,10 @@
+public class Encapsulation {
+    private int id;
+    String name;
+    public void setId(int id){
+        this.id=id;
+    }
+    public int getId(){
+        return id;
+    }
+}
